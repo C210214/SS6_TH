1aec032b1af3c6d9ad447453b72f923511c5ae46
c210214 <92442535+C210214@users.noreply.github.com>
2023-09-22 00:22:18 +0700

    first caaaaddit

--- src/TH3/Category.java
+++ src/TH3/Category.java
@@ -1,2 +1,15 @@
-package TH3;public class Category {
+package TH3;
+
+public class Category {
+    int categoryId;
+    String categoryName;
+    public Category() {}
+    public Category(int categoryId, String categoryName){
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+    public void display(){
+        System.out.println("Mã danh mục: " + categoryId + "\n"
+                + "Tên danh mục: " + categoryName);
+    }
 }
